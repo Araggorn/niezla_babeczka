e6ba40c4d2ecdc77e23b204714f07c99708d62d2
Araggorn <zerelik@gmail.com>
2021-04-03 19:29:30 +0200

    Start

---
+++ src/test/java/pl/olek/niezla_babeczka/NiezlaBabeczkaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.olek.niezla_babeczka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiezlaBabeczkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
